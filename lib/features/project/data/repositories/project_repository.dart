758d8f01811702114bc20ed6763b1f6d4b1d2dd9
CarlosRuidiaz <carlosandresruidiazmendoza@gmail.com>
2026-09-06 17:38:29 -0500

    feat(project): ProjectRepository sobre IProjectSource

---
+++ lib/features/project/data/repositories/project_repository.dart
@@ -0,0 +1,12 @@
+﻿import '../../domain/models/project.dart';
+import '../../domain/repositories/i_project_repository.dart';
+import '../datasources/i_project_source.dart';
+
+class ProjectRepository implements IProjectRepository {
+  ProjectRepository(this.source);
+
+  final IProjectSource source;
+
+  @override
+  Future<List<Project>> getProjects() async => await source.getProjects();
+}
