aec72d6516a8546037568ae5d5631f02e753e158
ClaudiaElias <cvelias@uninorte.edu.co>
2026-09-21 20:24:58 -0500

    feat(application): las postulaciones se leen y guardan en roble

--- lib/features/application/application_dependencies.dart
+++ lib/features/application/application_dependencies.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 import '../project/domain/repositories/i_project_repository.dart';
 import 'data/datasources/i_application_source.dart';
-import 'data/datasources/local/local_application_source.dart';
+import 'data/datasources/remote/roble_application_source.dart';
 import 'data/repositories/application_repository.dart';
 import 'domain/repositories/i_application_repository.dart';
 import 'ui/viewmodels/application_controller.dart';
@@ -12,7 +12,7 @@ import 'ui/viewmodels/application_controller.dart';
 /// aqui. `ManagementController` tambien resuelve `IApplicationRepository` de
 /// este registro.
 void registerApplication() {
-  Get.put<IApplicationSource>(LocalApplicationSource());
+  Get.put<IApplicationSource>(RobleApplicationSource(Get.find()));
   Get.put<IApplicationRepository>(ApplicationRepository(Get.find()));
   // fenix: GetX descarta el controlador al cerrarse la ruta que lo uso. Sin
   // esto, el segundo `Get.find` tras volver de la pantalla 14 lanza
